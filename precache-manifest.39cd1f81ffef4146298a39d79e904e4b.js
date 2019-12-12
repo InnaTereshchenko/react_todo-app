c36afa7b07da810ab3f8f230cd9eb44c08d49836
Inna <inna.tereshchenko1@gmail.com>
2019-12-12 21:33:53 +0200

    Updates

---
+++ precache-manifest.39cd1f81ffef4146298a39d79e904e4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ab79a4200f611cf8eb4dc8bd6908472",
+    "url": "/react_todo-app/index.html"
+  },
+  {
+    "revision": "a30646a36920ab70e92c",
+    "url": "/react_todo-app/static/css/main.62bbc3e1.chunk.css"
+  },
+  {
+    "revision": "8c1ca4089b4f1d17e5c4",
+    "url": "/react_todo-app/static/js/2.44edda1a.chunk.js"
+  },
+  {
+    "revision": "a30646a36920ab70e92c",
+    "url": "/react_todo-app/static/js/main.0efc5dd0.chunk.js"
+  },
+  {
+    "revision": "51af3b3c9c5f8ccb1c53",
+    "url": "/react_todo-app/static/js/runtime~main.df364381.js"
+  }
+]);
